01c9e1743b0b1948eab644a05dc2f92d536836ae
kientran2408 <52957723+kientran2408@users.noreply.github.com>
2019-08-31 15:12:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -63,12 +63,12 @@
   <!-- ABOUT -->
   <div class="w3-gray" id="about">
     <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="about">
-      <h2 class="w3-wide w3-center">About</h2>
+      <h2 class="w3-wide w3-center" style="color: black">About</h2>
       <br>
         <div class="w3-col m6 w3-padding-large w3-hide-small">
         <img src="images/Minato.jpg" class="w3-round w3-image w3-opacity-min" alt="minato" width="302" height="218">
         </div>
-        <h4><b>WHAT IS FIGURE?</b></h4>
+        <h4 style="color: black"><b>WHAT IS FIGURE?</b></h4>
         <p>Figure is a model, a statue, any 3D product that reproduces a character in a movie, story, game, or drawing.</p>
         <p>Usually figure bears the shape of a human, sometimes a beast, a god, a goblin, a monster or even an alien creature ... any character thought of by the artist, sculpted into 3D called the figure.</p>
         <br>
@@ -80,7 +80,7 @@
         <div class="w3-col m6 w3-padding-large w3-hide-small">
         <img src="images/kiritoasuna.jpg" class="w3-round w3-image w3-opacity-min" alt="coffee beans" width="302" height="218">
         </div>
-        <h4><b>WHAT IS JAPAN FIGURE?</b></h4>
+        <h4 style="color: black"><b>WHAT IS JAPAN FIGURE?</b></h4>
         <p>These are figures created by Japanese artists, illustrating, creating and finishing in Japan.</p>
         <p>Because of being born in Japan, it is obvious that the Japan figure has Japanese culture, from Anime, Manga, Game, Light novel, J-pop to Virtual Singer and so on.</p>
         <p>With Japanese technology and a strictly supervised production process, the figure products are very well-executed, sharp details, vivid colors, precise expressions like the original character have satisfied many fans. fans all over the world.</p>
@@ -91,7 +91,7 @@
   <!-- Contact Section -->
   <div class="w3-black" id="contact">
             <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
-                <h2 class="w3-wide w3-center">Contact</h2>
+                <h2 class="w3-wide w3-center" style="color: black">Contact</h2>
                     <div class="w3-row w3-padding-32">
                     <div class="w3-col m6 w3-large w3-margin-bottom">
                         <i class="fa fa-map-marker" style="width:30px"></i>Ha Noi, Vietnam<br>
